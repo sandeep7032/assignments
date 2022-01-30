a980b50209bac1553ffa169668d1b6fc25a3337c
sandeep7032 <95558185+sandeep7032@users.noreply.github.com>
2022-01-30 13:48:21 +0530

    Create index.js

---
+++ jan25/index.js
@@ -0,0 +1,35 @@
+assignment 1
+
+/*
+
+var num = prompt("enter the number");
+if (num <10){
+	console.log("number is less  than 10");
+}
+else if(num == 10){
+	console.log("number is equal 10");
+}
+else{
+	console.log("number is  greater than  10");
+}
+
+*/
+
+assignment 2
+
+var a = prompt("enter the first number");
+var b = prompt("enter the second number");
+var c = prompt("enter the third number");
+var a = parseInt(a);
+var b = parseInt(b);
+var c = parseInt(c);
+if (a>b && a>c){
+	console.log(a + " is maximum number");
+	}
+else if (b>a && b>c){
+	console.log(b + " is maximum number");
+	}
+else{
+	console.log(c + " is maximum number");
+	}
+console.log(typeof a);
